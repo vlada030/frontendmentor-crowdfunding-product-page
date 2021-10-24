1fa04027a84dd39ac7d03f8ef995f3d2539a3e19
Vlada DELL <vlada030@gmail.com>
2021-10-24 22:13:10 +0200

    postavljena navigacija

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './scss/main.scss'
+import "./scss/main.scss";
+
+const mobileMenu = document.querySelector(".nav-mobile");
+const nav = document.querySelector(".nav");
+const openMobileMenu = document.getElementById("openMobileMenu");
+const closeMobileMenu = document.getElementById("closeMobileMenu")
+
+    // open / close mobile menu
+    // [openMobileMenu, closeMobileMenu].addEventListener("click", () => {
+    //     mobileMenu.classList.toggle("open-modal");
+    // });
+
+    window.addEventListener('click', e => {        
+        if (e.target.closest('#openMobileMenu') || e.target.closest('#closeMobileMenu')) {
+            nav.classList.toggle('disabled')
+            mobileMenu.classList.toggle("open-modal");
+        }
+    })
